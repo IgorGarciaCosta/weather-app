ec9fd1cbe1d6f7d08c283d0a3b72cd622f859f73
IgorGarciaCosta <igonildo7@gmail.com>
2020-08-25 15:45:23 -0300

    geocode.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = 'http://api.weatherstack.com/current?access_key=e7156a5755c35521fce13aa751142bf7&query=37.8267,%20-122.4233'
 
@@ -21,19 +22,26 @@ const request = require('request')
 //geocoding
 //address ->lat/long->weather
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaWdvcmdjY3MiLCJhIjoiY2tkenhkczFqMXdyaDJybnNrZzV5ZWl3eCJ9.nuXzbRPhZ7Q2aPU2p6qsug&limit=1'
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaWdvcmdjY3MiLCJhIjoiY2tkenhkczFqMXdyaDJybnNrZzV5ZWl3eCJ9.nuXzbRPhZ7Q2aPU2p6qsug&limit=1'
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {//se não tem conexão
-        console.log('Unable to connect to Geocoding Service!')
-    }
-    else if (response.body.features.length === 0) {//se a URL for inválida
-        console.log('Unable to find location!')
-    }
-    else {//se der bom
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {//se não tem conexão
+//         console.log('Unable to connect to Geocoding Service!')
+//     }
+//     else if (response.body.features.length === 0) {//se a URL for inválida
+//         console.log('Unable to find location!')
+//     }
+//     else {//se der bom
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(latitude, longitude)
+//     }
 
+// })
+
+
+
+geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
