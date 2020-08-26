bf7074811f83aa8c1ffad59d898770a47f14aa64
IgorGarciaCosta <igonildo7@gmail.com>
2020-08-26 12:04:56 -0300

    user typed address

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-    //const lat = geocode.latitude
-})
 
-forecast(-71.0596, 42.3605, (error, data)=>{
-    console.log('Error', error)
-    console.log('Data', data)
-})
+
+// function readFromUser() {
+//     console.log('Enter the location: ');
+//     process.stdin.on('readable', () => {
+//         let typed = process.stdin.read();//read what is typed
+
+//         //trying to read
+//         typed = typed.toString().replace(/\n/, "");
+//         typed = typed.replace(/\r/, "");
+
+//         return typed;
+//     });
+// }
+
+const typedAddress = process.argv[2]
+
+if (!typedAddress) {
+    console.log('Please, provide some address')
+} else {
+    geocode(typedAddress, (error, data) => {
+
+        if (error) {
+            //uso o return pra parar a execução da função se der erro
+            return console.log(error)
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
+
+
+
+
+
