f1031229877af2b321abfd22a20b55cfd7c7f300
IgorGarciaCosta <igonildo7@gmail.com>
2020-09-02 22:32:35 -0300

    syncing forecast and geocode with server

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,9 +1,9 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=e7156a5755c35521fce13aa751142bf7&query='+latitude+',%20'+longitude+''
+    const url = 'http://api.weatherstack.com/current?access_key=e7156a5755c35521fce13aa751142bf7&query=' + latitude + ',%20' + longitude + ''
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable ot connect to weather service!', undefined)
         }
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' +body.current.feelslike + '% change of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.feelslike + '% change of rain.')
         }
     })
 
